00213eb9cd28d08649b77c16d551936190f3b248
Nick Gerner <nick.gerner@gmail.com>
2019-10-24 13:57:27 -0700

    basic craftorderlib structure

---
+++ data/scripts/entity/craftorders.lua
@@ -0,0 +1,32 @@
+CraftOrders.registerModdedCraftOrder(OrderType.AutoTrader, {
+    title = "AutoTrader",
+    callback = "onUserAutoTraderOrder"
+})
+
+function CraftOrders.onUserAutoTraderOrder()
+    if onClient() then
+        invokeServerFunction("onUserAutoTraderOrder")
+        ScriptUI():stopInteraction()
+        return
+    end
+
+    Entity():invokeFunction("data/scripts/entity/orderchain.lua", "clearAllOrders")
+    Entity():invokeFunction("data/scripts/entity/orderchain.lua", "addAutoTraderOrder", true)
+end
+callable(CraftOrders, "onUserAutoTraderOrder")
+
+function CraftOrders.autoTrader()
+    if onClient() then
+        invokeServerFunction("autoTrader")
+        ScriptUI():stopInteraction()
+        return
+    end
+
+    if checkCaptain() then
+        CraftOrders.removeSpecialOrders()
+
+        -- Entity():addScriptOnce("ai/autoTrader.lua")
+        return true
+    end 
+end
+callable(CraftOders, "autoTrader")
---
+++ data/scripts/lib/ordertypes.lua
@@ -0,0 +1,6 @@
+
+registerModdedOrderType("AutoTrader", {
+    name = "AutoTrader"%_T,
+    icon = "data/textures/icons/autotrader.png",
+    pixelIcon = "data/textures/icons/pixel/autotrader.png",
+})
