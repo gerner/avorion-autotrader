85f43b7ddc75bd6a4f8738710026c9df837beb23
Nick Gerner <nick.gerner@gmail.com>
2019-10-29 16:53:49 -0700

    setting vanilla version filters

--- modinfo.lua
+++ modinfo.lua
@@ -40,7 +40,7 @@ meta =
     -- },
     dependencies = {
         {id = "1837703287", min = "1.1"},
-        {id = "Avorion", max = "0.*"}
+        {id = "Avorion", min = "0.25", max = "0.27"}
     },
 
     -- Set to true if the mod only has to run on the server. Clients will get notified that the mod is running on the server, but they won't download it to themselves
